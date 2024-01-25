c5da4b039e429cf0331c3735233c61758568e732
Klivchenko-Artem <solo30393@gmail.com>
2024-01-25 13:30:10 +0700

    first

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+function Header() {
+  return (
+    <nav className="light-blue">
+      <div className="nav-wrapper">
+        <a href="!#" className="brand-logo">
+          Logo
+        </a>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li>
+            <a href="!#">repo</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
